117786360fe5be3fb8c673a76e6eca261e045ebb
qingzhifan <2082495024@qq.com>
2019-03-17 23:06:38 +0800

    作业

---
+++ yychatclient/src/com/yychat/model/User.java
@@ -0,0 +1,20 @@
+package com.yychat.model;
+
+public class User {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void setPassWord(String passWord) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setUserName(String userName) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
